42a85296cb31f2e317ecf6338855a0a5422059d9
ArcotHemanthRayan <97214947+ArcotHemanthRayan@users.noreply.github.com>
2022-02-17 10:13:19 +0530

    Add files via upload

---
+++ Phase-3 Projects/Phase-3 Assisted Projects/MicroServicesOne/src/main/java/com/spring/MicroServicesOneApplication.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicesOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicesOneApplication.class, args);
+	}
+
+}
