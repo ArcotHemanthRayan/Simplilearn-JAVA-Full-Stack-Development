03ab4fe1d5e1bc1d73d2d9f2e426b089676c751d
ArcotHemanthRayan <97214947+ArcotHemanthRayan@users.noreply.github.com>
2022-03-19 09:20:03 +0530

    Add files via upload

---
+++ Phase-5 Projects/Main Project/CI-CD Deployment for Springboot Application/src/test/java/com/phase5/project/MyApplicationTests.java
@@ -0,0 +1,12 @@
+package com.phase5.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
